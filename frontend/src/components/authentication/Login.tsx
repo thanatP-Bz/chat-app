02d81d364b15ccbcd14c2f204587951ca2becfb1
Thanat <bztzn512@gmail.com>
2023-11-15 17:53:42 -0500

    addSubmitHandler

--- frontend/src/components/authentication/Login.tsx
+++ frontend/src/components/authentication/Login.tsx
@@ -1,11 +1,78 @@
-import React from "react";
-import { Container, Box } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Button,
+  InputGroup,
+  InputRightElement,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import { FormControl, FormLabel } from "@chakra-ui/react";
 
 const Login = () => {
+  const [show, setShow] = useState<boolean>(false);
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [pic, setPic] = useState<string>("");
+  const [picLoading, setPicLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const toast = useToast();
+
+  const handleClick = () => setShow(!show);
+
+  const submitHandler = async () => {
+    setLoading(true);
+  };
+
   return (
-    <Container maxW="xl" centerContent>
-      <Box></Box>
-    </Container>
+    <VStack spacing="10px">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          value={email}
+          type="email"
+          placeholder="Enter Your Email Address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={loading}
+      >
+        Login
+      </Button>
+      <Button
+        variant="solid"
+        colorScheme="red"
+        width="100%"
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("123456");
+        }}
+      >
+        Get Guest User Credentials
+      </Button>
+    </VStack>
   );
 };
 
