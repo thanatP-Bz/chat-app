5d423ea3aad2b385f5035068014793d7dca6b3ae
Thanat <bztzn512@gmail.com>
2023-12-04 19:00:28 -0500

    IcanMakeIt!!!!

--- frontend/src/context/ChatProvider.tsx
+++ frontend/src/context/ChatProvider.tsx
@@ -4,25 +4,49 @@ import {
   useState,
   useEffect,
   ReactNode,
+  Dispatch,
+  SetStateAction,
 } from "react";
 import { useNavigate } from "react-router-dom";
 
-interface IContextProps {
-  user: string | null;
+type User = {
+  _id: string;
+  name: string;
+  pic: string;
+  email: string;
+};
+
+interface UserContextInterface {
+  user: User;
+  setUser: Dispatch<SetStateAction<User>>;
 }
 
-const ChatContext = createContext({} as IContextProps);
+const defaultState = {
+  user: {
+    _id: "",
+    name: "",
+    pic: "",
+    email: "",
+  },
+} as UserContextInterface;
+
+const ChatContext = createContext(defaultState);
 
 type Children = {
   children: ReactNode;
 };
 
 const ChatProvider = ({ children }: Children) => {
-  const [user, setUser] = useState<string | null>("");
+  const [user, setUser] = useState<User>({
+    _id: "",
+    name: "",
+    pic: "",
+    email: "test@gmail.com",
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
-    const userInfo: string | null = localStorage.getItem("userInfo");
+    const userInfo = JSON.parse(localStorage.getItem("userInfo") || "");
     setUser(userInfo);
 
     if (!userInfo) {
@@ -31,7 +55,9 @@ const ChatProvider = ({ children }: Children) => {
   }, [navigate]);
 
   return (
-    <ChatContext.Provider value={{ user }}>{children}</ChatContext.Provider>
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
   );
 };
 
